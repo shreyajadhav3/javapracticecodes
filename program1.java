9bdfe45dffe50ca8bab4f49adab9923f8928a390
shreya <shreya00say1@gmail.com>
2026-04-04 18:42:11 +0530

    java code 1 added

---
+++ program1.java
@@ -0,0 +1,11 @@
+class Demo{
+public static void main(String[]args){
+System.out.println("in main");
+Codex.fun();
+}
+}
+class Codex{
+public static void fun(){
+System.out.println("in codex");
+}
+}
