afd2def51dfbd68367ca745ef496e89859a8b4b5
shreya <shreya00say1@gmail.com>
2026-04-06 00:33:14 +0530

    SOP line parallel understanding code

---
+++ practice2.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String[] args){
+		System.out.println("in Demo main");
+			Codex. Chavi. course();
+			
+	}
+}
+class Codex{
+	static ICS Chavi = new ICS();
+}
+class ICS{
+	void course(){
+		System.out.println("jfs-35k");
+	}
+}
