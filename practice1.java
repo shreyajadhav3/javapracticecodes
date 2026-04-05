4fe059f1b8ea6bdb9856d8c9bbae4b9d842ef82f
shreya <shreya00say1@gmail.com>
2026-04-06 00:08:35 +0530

    code of static and non-static in one class

---
+++ practice1.java
@@ -0,0 +1,19 @@
+class Demo{
+	public static void main(String[] args){
+		System.out.println("in demo main");
+			Codex Chavi = new Codex();
+				Chavi.fun();
+					Chavi.gun();			
+	}
+}
+
+class Codex{
+	void fun(){
+		System.out.println("in codex fun");
+		}
+
+	static void gun(){
+		System.out.println("in codex gun");
+	}
+}	
+
