53217618ccb0002aa31a412edda050d07a13296b
shreya <shreya00say1@gmail.com>
2026-04-12 00:02:24 +0530

    integer type scope expln code

---
+++ p1.java
@@ -0,0 +1,14 @@
+class Demo{
+	public static void main(String[]args){
+	int a = 20;
+	System.out.println(a);
+		Codex.gun();
+	}
+}
+
+class Codex{
+	public static void gun(){
+	int a = 70;
+	System.out.println(a);
+	}	
+}
